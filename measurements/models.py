e01299a5ea76344542ba8279dd0a8431fe33c8d9
IvanovSemjon <IvanovSemjon@yabdex.ru>
2025-08-22 10:26:05 +0300

    Выполнение домашнего задания с использованием ДРФ

---
+++ measurements/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class Project(models.Model):
+    """Объект на котором проводят измерения."""
+
+    name = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.latitude}, {self.longitude})"
+
+
+class Measurement(models.Model):
+    """Измерение температуры на объекте."""
+
+    value = models.FloatField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return f"{self.value} ({self.project.name})"
